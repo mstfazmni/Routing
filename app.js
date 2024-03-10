92476000afbfc3f5bbe5f04b3ecc29d35a23cd84
Mostafa Zamani <119905696+mstfazmni@users.noreply.github.com>
2024-03-10 15:38:30 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const routes = require('./routes');
+
+const PORT = process.env.PORT || 3000;
+
+app.use('/', routes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
